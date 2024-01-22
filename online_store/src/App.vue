89ca25a30d317b76dd3980ed2aded15821416db4
angelAntezana <angel2estevan@gmail.com>
2024-01-22 13:35:02 +0100

    Create front

---
+++ online_store/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<template>
+  <header>
+    <div class="">
+      <nav class="flex
+      justify-center
+      items-center
+      gap-8
+      p-6
+    bg-gray-800
+    text-white
+      text-center">
+        <RouterLink class="w-24 bg-red-700" to="/">Home</RouterLink>
+        <RouterLink class="w-24 bg-red-700" to="/store">Store</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+<style scoped>
+.custom-nav {
+  background-color: red;
+  width: 100%;
+  height: 100px;
+  padding: 20px 20px;
+}
+</style>
---
+++ online_store/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <header class="flex justify-center">HOME VIEW</header>
+  </main>
+</template>
+<style scoped>
+.custom-header {
+  background-color: aqua;
+}
+</style>
